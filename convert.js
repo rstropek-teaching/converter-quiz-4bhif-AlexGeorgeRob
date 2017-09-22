1f6a262d91a82458039a9f6fcc46ab0c69d3f3bb
Rainer Stropek <rainer@timecockpit.com>
2017-09-22 07:50:53 +0200

    Template string

--- convert.js
+++ convert.js
@@ -3,4 +3,5 @@ weights = {"g" : 1, "kg" : 1000};
 if(6 != process.argv.length || isNaN(process.argv[2]) || process.argv[4] != "to" || (!metrics[process.argv[3]] && !weights[process.argv[3]]) || (!metrics[process.argv[5]] && !weights[process.argv[5]]) || (metrics[process.argv[3]] && weights[process.argv[5]]) || (weights[process.argv[3]] && metrics[process.argv[5]]))
     console.log("Invalid parameters");
 else
+    // Consider using a template string instead
     console.log(process.argv[2] + " " + process.argv[3] + " are " + (process.argv[2] * (metrics[process.argv[3]] ? metrics[process.argv[3]] : weights[process.argv[3]]) / (metrics[process.argv[5]] ? metrics[process.argv[5]] : weights[process.argv[5]]) + " " + process.argv[5]));
